c9286dedc71cc2765dcc905acd1f737864598dbc
Newphone <muhammadfahri190713@gmail.com>
2026-07-26 08:13:19 +0700

    Initialize main script for auto ban process

---
+++ index.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+#!/usr/bin/env node
+
+require('dotenv').config();
+const chalk = require('chalk');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+
+const BanService = require('./lib/BanService');
+const PhoneFormatter = require('./lib/PhoneFormatter');
+const UIRenderer = require('./lib/UIRenderer');
+const Logger = require('./lib/Logger');
+
+const logger = new Logger();
+
+async function main() {
+  UIRenderer.showBanner();
+  
+  try {
+    // Input phone number
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'phoneNumber',
+        message: chalk.red('➜ Enter target phone number (62... or +62...):'),
+        validate: (input) => {
+          const formatted = PhoneFormatter.format(input);
+          if (formatted && formatted.length >= 10 && formatted.length <= 15) {
+            return true;
+          }
+          return chalk.red('Invalid phone number format');
+        },
+      },
+    ]);
+    
+    const targetNumber = PhoneFormatter.format(answers.phoneNumber);
+    
+    UIRenderer.showHeader(targetNumber);
+    
+    logger.info('Starting auto ban process...');
+    
+    // Initialize ban service
+    const banService = new BanService();
+    
+    // Execute all ban methods
+    const results = await banService.executeAllMethods(targetNumber);
+    
+    UIRenderer.showSummary(results, targetNumber);
+    
+    // Ask to continue
+    const continueAnswer = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'continue',
+        message: chalk.red('Ban another account?'),
+        default: false,
+      },
+    ]);
+    
+    if (continueAnswer.continue) {
+      console.log('');
+      await main();
+    } else {
+      logger.success('Exiting AutoBan. Goodbye!');
+      process.exit(0);
+    }
+  } catch (error) {
+    logger.error('Fatal error: ' + error.message);
+    process.exit(1);
+  }
+}
+
+// Handle uncaught errors
+process.on('uncaughtException', (error) => {
+  logger.error('Uncaught Exception: ' + error.message);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error('Unhandled Rejection: ' + reason);
+  process.exit(1);
+});
+
+// Run main
+main();
+      
